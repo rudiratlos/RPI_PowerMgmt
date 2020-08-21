90e1360843de6c7404f9ada553ff2fa2f7631d83
Rudi Ratlos <rudiratlos63@yahoo.com>
2020-08-21 16:02:50 +0200

    V5.0

--- RPI_ShutDownReboot.ino
+++ RPI_ShutDownReboot.ino
@@ -1,36 +1,31 @@
-//PumpShutdownReboot Version 4.1 Arduino IDE Sketch 
-#undef RelaisPinToPB1
-#undef DigiSpark4Test
-/*
+/* PumpShutdownReboot Version 5.0 Arduino IDE Sketch
 Shutdown/Reboot function for RPI, with Voltage observation and
    Status signalling with BiColor LED with 3 Pins (red/green/GND).
    Use 330Ohm Resistor between LED Cathode and GND
    - 5V comes from Powersupply  
-   - 3.3V comes from Raspberry PI Header Pin1, voltage will be analyzed
+   - 3.3V comes from Raspberry PI Header Pin 1, voltage will be analyzed
    - Push-Button shorts to GND and is connected to ATTiny PB3 and a RPI GPIO IN
      (do not use external PullUps, both will/should use internal PullUps).
      1sec: Relais will be switched ON
      3sec: Relais will be switched OFF after 10sec
    - Relais OUT (use Transistor driver) which will 
-     supply RPI backpowered on Pins 2&4 with 5V
+     supply 5V to RPI (backpower) on rpi HDR-P1 Pins 2&4
 
 Functions:
     - LED dimming RED:    Relais is OFF, 3.3V not in allowed range -> 0V
     - LED steady  RED:    Relais is ON,  3.3V not in allowed range
-    - LED Blink   RED:    Under-/Overvoltage/Overheat detection
+    - LED blink   RED:    Under-/Overvoltage/Overheat detection
                             Relais state and overvoltage condition:
                               is ON:  Relais will switch off immediatly
                               is OFF: Relais will not switch on
-    - LED dimming GREEN:  Relais is ON:  Voltage was/is below 3.3V
-                            will be reset to steady Green after some secs
-                            e.g. indicate Powersupply is not stable
-    - LED steady  GREEN:  Relais is ON:  3.3V in allowed range
+    - LED steady  GREEN:  Relais is ON: 3.3V in allowed range
+    - LED blink   GREEN:  Relais in ON: during Bootprocess
 
      ------------- ATtiny85 (5.5V Version) --------------
                             +---v---+
            (Relais Out) PB5 |1     8| VCC (5V from Powersupply)
-           (/Button IN) PB3 |2     7| PB2 (AnalogIN 3.3V RPI Hdr-P1/Pin1)
-    (LED Anode Red OUT) PB4 |3     6| PB1 (DO NOT CONNECT, reserved future use)
+           (/Button IN) PB3 |2     7| PB2 (AnalogIN 3.3V RPI HDR-P1/Pin1)
+    (LED Anode Red OUT) PB4 |3     6| PB1 (OUT PWM backlight LED TFT display)
                         GND |4     5| PB0 (OUT LED Anode Green)
                             +-------+
      -------------------------------------------------------
@@ -40,35 +35,29 @@ Functions:
      RSTDISBL=1      (enable)  using PB5 as I/O-Pin (hfuse)
      !!!! Chip can not be reprogrammed -> reset FUSE by HighVoltage programmer
 
-   avrdude -F -p attiny45 -P usb -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:<file2flash>.hex
+   avrdude -F -p attiny85 -P usb -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:<file2flash>.hex
 
-   install ATTiny45/85 Tool:
+   install ATTiny85 Tool:
    https://www.kollino.de/arduino/attiny-support-unter-arduino-1-6-installieren/
-
    http://drazzy.com/package_drazzy.com_index.json
 
    use Arduino IDE to generate .hex flash file to program ATtiny 
    
    Tools Setup Tab (BoardManagement):
    Board: ATtiny25/45/85 (No bootloader)
-   Chip:  ATtiny45 or ATtiny85
+   Chip:  ATtiny85
    Clock: 8 MHz (internal)
 */
 
-// PINdefinition
-  const byte ButtonPIN =   PB3;
-  const byte LEDgreenPIN = PB0;
- #ifdef DigiSpark4Test 
-  const byte LEDredPIN =   PB1;
-  const byte RelaisPIN =   PB5; 
- #else 
-  const byte LEDredPIN =   PB4;
-  #ifdef RelaisPinToPB1 
-    const byte RelaisPIN = PB1; 
-  #else 
-    const byte RelaisPIN = PB5; 
-  #endif
-#endif
+//PINdefinition
+  const byte LEDgreenPIN = PB0; // Pin5
+  const byte PWMpin =      PB1; // Pin6 
+  const byte ADCpin =      PB2; // Pin7
+  const byte ButtonPIN =   PB3; // Pin2
+  const byte LEDredPIN =   PB4; // Pin3 
+  const byte RelaisPIN =   PB5; // Pin1
+ 
+  const long SerBaud=    19200; // 9600, 19200, 38400, 57600, 115200
 
 //ADC-Values
   const float BGtolD=     0.75; // ScaleDown, due to ATtiny85 BandGap tolerance
@@ -79,7 +68,7 @@ Functions:
   const float VCCokVolt=  5.250-ShottkyFwdV; // optimal supply voltage
   const float VCCmin=    (4.750*BGtolD)-ShottkyFwdV;   // 3.992 min expected
   
-//Regulator NCP1117-3.3V:   // min. Vout: 3.235 // max. Vout: 3.365
+//Regulator   NCP1117-3.3V: // min. Vout: 3.235 // max. Vout: 3.365
   const float V33max=     3.365*BGtolU; // 3.7015
   const float V33min=     3.235*BGtolD; // 2.9115
   const float V33okVolt=  V33min+((V33max-V33min)/2);
@@ -92,16 +81,21 @@ Functions:
   const byte  pwmsteady=    PWMtabmax;     // idx: pwm value for LED steady
   const byte  pwmlow=       26;            // idx: lower intensity level (dimming)
   const byte  FadeCntMax=   4;             // delay cnt between loading next pwm level
-
+  const byte  PWMdflt=      244;           // default PWM level for display backlight ctrl
+                                           // (1.8TFT VCC:5.55V 244:3.37V@58mA 255:3.44V@60mA)
 //time counter divideable by 'wtim'
   const byte  wtim=            10;         // debounce time (msec). Processor runs @ 8MHz
+  const word  cnt100ms=       100 / wtim;
+  const word  cnt250ms=       250 / wtim;
+  const word  cnt1sec=       1000 / wtim;
   const word  cntBootUp=      990 / wtim;  // 1sec Button has to be pressed: Bootstrap
   const word  cntShutDwn=    2980 / wtim;  // 3sec Button has to be pressed: ShutDown
 //rpi_hal has 3000msec / 1000msec and 7msec debounce time
   const word  cntoffmax=    10000 / wtim;  //  10sec waittime for RPI shutdown-/boot-process
   const word  cntPwrENAmin=     3 * wtim;  // minimum 36msec PwrEna
   const word  cntPwrENAmax=  2980 / wtim;  // maximum  3 sec PwrEna
-  const word  cntBootTime=  10000 / wtim;  // suppose 10sec rpi BootTime
+  const word  cntBootTime1=  3000 / wtim;  // suppose 3sec rpi BootTime phase 1
+  const word  cntBootTime2=  7000 / wtim;  // suppose 7sec rpi BootTime phase 2
   const word  cntGetVREF=   60000 / wtim;  // get VREF every 60sec
   const word  cntresetVfail= cntoffmax;
   const word  cntLEDoffmax=  1500 / wtim;  // 1.5sec offtime @ dimming and idx =0
@@ -113,32 +107,101 @@ Functions:
   const byte  LEDoff=0, LEDon=1, LEDdim=2; // mode/state
   
 //rpiSTATEs:
-  const byte  rpiNEW=0,      rpiOFF=1, rpiSwitchOff=2, rpiSwitchOn=3;
-  const byte  rpiDOboot=4,   rpiON=5,  rpiDOreboot=6,  rpiDOshutdown=7;
-  const byte  rpiAlarmOFF=8;
+  const byte  rpiNEW=0,      rpiOFF=1,        rpiSwitchOff=2, rpiSwitchOn=3;
+  const byte  rpiDOboot=4,   rpiDOboot2=5,    rpiON=6;
+  const byte  rpiDOreboot=7, rpiDOshutdown=8, rpiAlarmOFF=9;
 
-  bool  RelaisStat=false,PWMup=true,BUTpres=false,Blink=false;
+  const byte  eeprom_pattern=0xf0;
+
+  bool  PWMup=true,BUTpres=false,Blink=false;
   int   VCCstat=0,VCCstatLast=-2,V33stat=0,V33statLast=-2,TEMPstat=0,TEMPstatLast=-2;
   byte  rpiSTATE=rpiOFF,rpiSTATElast=rpiNEW,LEDstate=rpiSTATE,LED=LEDredPIN;
-  byte  PWMidx=0,PWMmax=PWMtabmax,FadeCnt=FadeCntMax;
+  byte  RelaisStat=LOW,PWMidx=0,PWMmax=PWMtabmax,FadeCnt=FadeCntMax,PWMoutLvl=PWMdflt;
   word  BGraw,BG2raw,V33raw;
-  word  cntPwrENA=0,BlinkCnt=0,BlinkMax=cntblinkmaxc,ERRcnt=0,LEDoffCnt=0;
+  word  cntPwrENA=0,BlinkCnt=0,BlinkMax=cntblinkmaxc,ERRcnt=0,LEDoffCnt=0,gcnt=0;
   float VREF=VCCokVolt,V33=V33okVolt,BGd=1.0;
 
-void TinyInit() {
-  pinMode(LEDgreenPIN, OUTPUT);
-  pinMode(LEDredPIN,   OUTPUT);
-  pinMode(RelaisPIN,   OUTPUT); 
-  pinMode(ButtonPIN,   INPUT_PULLUP);
+#include <EEPROM.h>
+#include <SoftwareSerial.h> 
+SoftwareSerial swSer(ButtonPIN, LEDgreenPIN); // RX, TX  
+
+/*void blk(byte cnt)  {
+  for (byte _i= 1; _i<=cnt; _i++) {
+    digitalWrite(LEDgreenPIN, HIGH);
+    delay(500);
+    digitalWrite(LEDgreenPIN, LOW); 
+    delay(500);
+  }
+}*/
+
+void EEPROMsave() {
+  EEPROM.write(0, eeprom_pattern);
+  EEPROM.write(2, PWMoutLvl);
 }
 
-void SetRelais(bool ein) {
-  if (ein)
-       digitalWrite(RelaisPIN, HIGH);
-  else digitalWrite(RelaisPIN, LOW);
+void EEPROMread() {
+  if (EEPROM.read(0) != eeprom_pattern) { 
+    PWMoutLvl=PWMdflt;
+    EEPROMsave(); 
+  } else PWMoutLvl= EEPROM.read(2);
+}
+
+void InitVARs() {
+  LEDoffCnt= 0; FadeCnt= 0;  gcnt=   0;
+  cntPwrENA= 0; BlinkCnt= 0; ERRcnt= 0;
+}
+
+void Input_Read() { BUTpres=(digitalRead(ButtonPIN)==LOW); } // active low
+
+void SetRelais(byte relstat) {
+  InitVARs();
+  digitalWrite(RelaisPIN, relstat);  
+  if (RelaisStat!=relstat) delay(500); // let environment stabilize
+  RelaisStat= relstat;
+}
+
+void SetPWMout(byte PWMval) { analogWrite(PWMpin, PWMval); }
+
+void TinyInit() {
+  swSer.begin(SerBaud);
+  swSer.setTimeout(200);
+
+  pinMode(ButtonPIN,   INPUT_PULLUP);
   
-  if (RelaisStat!=ein) delay(500); // let environment stabilize
-  RelaisStat= ein;
+  pinMode(LEDgreenPIN, OUTPUT);
+  pinMode(LEDredPIN,   OUTPUT);
+  
+//pinMode(ADCpin,      INPUT);
+ 
+  pinMode(RelaisPIN,   OUTPUT); 
+  RelaisStat=HIGH; // force delay
+  SetRelais(LOW);  // switch off
+
+  pinMode(PWMpin,      OUTPUT);
+  SetPWMout(0);
+  EEPROMread();
+}
+
+void WriteLED(byte lednr, byte PWMval) {
+  switch (lednr) {
+    case LEDredPIN:
+           analogWrite(LEDredPIN, PWMval);      
+         break; 
+    case LEDgreenPIN:
+           if (PWMval>=0x80) 
+                digitalWrite(LEDgreenPIN, HIGH); 
+           else digitalWrite(LEDgreenPIN, LOW);
+         break;
+  } // switch
+}
+
+void SetLED(byte lednr, byte PWMmaxidx) {
+  LED= lednr; PWMmax= PWMmaxidx;
+  switch (lednr) { // switch other LED off  
+    case LEDredPIN:   WriteLED(LEDgreenPIN, PWMtab[0]); break;      
+    case LEDgreenPIN: WriteLED(LEDredPIN,   PWMtab[0]); break;
+  } // switch
+  WriteLED(lednr, PWMtab[0]); // set LED to PWM level
 }
 
 void SetPWMidx(byte mode) {
@@ -159,7 +222,8 @@ void SetPWMidx(byte mode) {
                   } else PWMidx++;
                 } else { // down
                   if (PWMidx<=0) {
-                    if (RelaisStat || (LEDoffCnt >= cntLEDoffmax)) {
+                    if ((RelaisStat==HIGH) || 
+                       (LEDoffCnt >= cntLEDoffmax)) {
                       PWMup=  true; 
                       PWMidx= 1; 
                       LEDoffCnt=   0;
@@ -170,20 +234,7 @@ void SetPWMidx(byte mode) {
             break;
   } // switch
 
-  if (FadeCnt==0) analogWrite(LED, PWMtab[PWMidx]);
-}
-
-void SetLED(byte lednr, byte PWMmaxidx) {
-  LED= lednr; PWMmax= PWMmaxidx;
-  switch (LED) {
-    case LEDredPIN: // red LED in PWM mode, green switch off
-             analogWrite(LEDgreenPIN, PWMtab[0]);           
-         break; 
-    case LEDgreenPIN: // green LED in PWM mode, red switch off
-             analogWrite(LEDredPIN,   PWMtab[0]);  
-         break;
-  } // switch
-  analogWrite(LED, PWMtab[0]); 
+  if (FadeCnt==0) WriteLED(LED, PWMtab[PWMidx]);
 }
 
 void StartBlink(bool alarm) {
@@ -217,7 +268,7 @@ void SetLED4status() {
     else BlinkCnt++;  
     
   } else { // dimming or steady on
-    if (RelaisStat)
+    if (RelaisStat==HIGH)
          SetPWMidx(LEDon);  // steady ON 
     else SetPWMidx(LEDdim); // RPI in OFF Status (LED PWM mode)
   }
@@ -230,7 +281,7 @@ void SetupLED() {
     StartBlink(true);
   } else {
     StopBlink();
-    if (RelaisStat) { // Relais ON
+    if (RelaisStat==HIGH) { // Relais ON
       if (( rpiSTATE==rpiDOshutdown) || (rpiSTATE==rpiDOreboot)) {
         if (rpiSTATE==rpiDOshutdown) 
              SetLED(LEDredPIN,  pwmsteady);
@@ -241,7 +292,8 @@ void SetupLED() {
              SetLED(LEDgreenPIN,pwmsteady);       // Voltage in OK range
         else SetLED(LEDredPIN,  pwmsteady);       // Voltage not OK
         
-        if (rpiSTATE==rpiDOboot) StartBlink(false);
+        if (rpiSTATE==rpiDOboot)  StartBlink(true);
+        if (rpiSTATE==rpiDOboot2) StartBlink(false);
       }
     } 
     else { // Relais OFF
@@ -305,7 +357,7 @@ word _raw=0;
            else BGd= 1.0;
          break;
     default: // ADC connected to PB2, meassure 3.3V from rpi
-           if (RelaisStat) {
+           if (RelaisStat==HIGH) {
              AD_ReadRAW(_loops,V33raw);
              V33= V33raw * VREF / 1024.0;
            } else V33= 0; // RelaisOFF: PB2 is open, we get unreliable values
@@ -362,72 +414,69 @@ byte _state;
   LEDstate=_state;
 }
 
-void InitVARs() {
-  LEDoffCnt= 0; FadeCnt= 0;
-  cntPwrENA= 0; BlinkCnt= 0; ERRcnt= 0;
-}
-
-void Input_Read() {
-  BUTpres=(digitalRead(ButtonPIN)==LOW); // active low
-}
-
 void DoRelais(bool stat) {
-word _gcnt;
   if (stat) { // switch Relais OFF
     
     if (rpiSTATE == rpiAlarmOFF) 
-         _gcnt= cntoffmax; // prevent loop, immediate Relais OFF
-    else _gcnt= 0;
+         gcnt= cntoffmax; // prevent loop, immediate Relais OFF
+    else gcnt= 0;
     
     rpiSTATE= rpiDOshutdown;
       
-    while (_gcnt < cntoffmax) { // RPI shutdown process (>10sec)
-      if ((_gcnt % 100)==0) GetVREF(); // every 1 sec
+    while (gcnt < cntoffmax) { // RPI shutdown process (>10sec)
+      if ((gcnt % cnt1sec)==0) GetVREF(); // every 1 sec
       if ((VCCstat<=0) && (TEMPstat<=0)) {
         SetLED4status();
         delay(wtim);   
-      } else _gcnt= cntoffmax;
-      _gcnt++;
+      } else gcnt= cntoffmax;
+      gcnt++;
     }
 
-    SetRelais(false);
-    InitVARs();
+    SetPWMout(0);
+ 
+    SetRelais(LOW);
     rpiSTATE= rpiOFF;
     
   } else { // switch Relais ON
     GetVREF();
     if ((VCCstat!=1) && (TEMPstat!=1)) {
 //    prevent Relais ON
-      SetRelais(true);
-      InitVARs();
+      SetRelais(HIGH);
       rpiSTATE= rpiDOboot;
+      EEPROMread();
     }
   }
   GetVREF();
   SetupLED();
 }
 
-void rpiNextSTATE(byte state, byte nextstate, word setOncount, word &counter) {
-  if (rpiSTATE==state) {
-    if (counter >= setOncount) {
+bool NextSTATE(byte state, byte nextstate, byte &setstate, word SetOnCount, word &counter) {
+bool _ok=false;
+  if (setstate==state) {
+    if (counter>=SetOnCount) {
       counter= 0;
-      rpiSTATE=nextstate;
+      setstate=nextstate;
+      _ok= true;
      } else counter++;
   }
+  return(_ok);
 }
 
 void STATEmachine() {
   if ((VCCstat>0) || (V33stat>0) || (TEMPstat>0)) {
 //  VCC or RPI 3.3V overvoltage -> Relais OFF
-    if (RelaisStat) rpiSTATE=rpiAlarmOFF;
+    if (RelaisStat==HIGH) rpiSTATE=rpiAlarmOFF;
   } else {
     if (BUTpres) {
-      rpiNextSTATE(rpiOFF,      rpiSwitchOn,   cntBootUp,    cntPwrENA);
-      rpiNextSTATE(rpiON,       rpiDOshutdown, cntShutDwn,   cntPwrENA);
+      NextSTATE(rpiOFF,      rpiSwitchOn,   rpiSTATE, cntBootUp,    cntPwrENA);
+      NextSTATE(rpiON,       rpiDOshutdown, rpiSTATE, cntShutDwn,   cntPwrENA);
     } else {
-      rpiNextSTATE(rpiDOboot,   rpiON,         cntBootTime,  cntPwrENA);  //15secBootTim
-      rpiNextSTATE(rpiDOreboot, rpiDOboot,     cntPwrENAmax, cntPwrENA);  //3sec->reset
-      if ((rpiSTATE==rpiOFF) || (rpiSTATE==rpiON))           cntPwrENA=0; //cncl BUTpres
+ //   during Boot phases
+      NextSTATE(rpiDOboot,   rpiDOboot2,    rpiSTATE, cntBootTime1, cntPwrENA); //3sec
+      NextSTATE(rpiDOboot2,  rpiON,         rpiSTATE, cntBootTime2, cntPwrENA); //7sec
+      
+      NextSTATE(rpiDOreboot, rpiDOboot,     rpiSTATE, cntPwrENAmax, cntPwrENA);  //3sec->reset
+      if ((rpiSTATE==rpiOFF) || (rpiSTATE==rpiON))                  cntPwrENA=0; //cncl BUTpres
     }
   }
 
@@ -438,21 +487,46 @@ void STATEmachine() {
     case rpiSwitchOn:   DoRelais(false); break;
     case rpiDOshutdown: DoRelais(true);  break;
     case rpiAlarmOFF:   DoRelais(true);  break;
+    case rpiDOboot2:    SetPWMout(PWMoutLvl); break;
   } // switch
 }
 
 void setup() {
   TinyInit();
-  InitVARs();
-  SetRelais(false); 
-  delay(500);
 }
 
 void loop() {
     Input_Read();
-    if (!BUTpres) GetVREF();
+    
+    if (gcnt>=cnt1sec) {
+      if (!BUTpres) {
+        GetVREF();
+      }
+      gcnt= 0;
+    } else {
+       if ((gcnt % cnt100ms)==0) {
+         if (swSer.peek()!=-1) {
+           String str = swSer.readStringUntil('\n');
+           str.toUpperCase(); str.trim();
+
+           if (str.indexOf("SHUT")>=0) rpiSTATE= rpiDOshutdown;
+           if (str.indexOf("BOOT")>=0) rpiSTATE= rpiSwitchOn;
+           if (str.indexOf("ESAV")>=0) EEPROMsave();
+           
+           if (str.indexOf("DIM")>=0) {
+             str.replace("DIM",""); str.trim();
+               if (str.length()>0) {
+                 PWMoutLvl= str.toInt();
+                 SetPWMout(PWMoutLvl);  
+               } 
+           }
+         }
+       }
+    }
+    
     STATEmachine();
     SetLED4status();
+    gcnt++;
     delay(wtim);
 }
 
--- avrdude.sh
+++ avrdude.sh
@@ -6,14 +6,14 @@
 # apt update
 # apt -qy install avrdude
 #
-echo "ATTiny45 align to socket bottom"
+echo "ATTiny85 align to socket bottom"
 #
 echo "hfuse burn reset disabled (RSTDISBL), ATtiny45 can not be reprogrammed (or use a high voltage programmer). Use it for burning a production chip"
-avrdude -F -p attiny45 -P /dev/ttyACM0 -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
-#avrdude -F -p attiny45 -P usb -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
+avrdude -F -p attiny85 -P /dev/ttyACM0 -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
+#avrdude -F -p attiny85 -P usb -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
 #
 #echo "hfuse burn reset enabled (no RELAIS output on Pin1 PB5), just for testing, chip can be reused"
-#avrdude -F -p attiny45 -P /dev/ttyACM0 -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
+#avrdude -F -p attiny85 -P /dev/ttyACM0 -c stk500V2 -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U flash:w:/usr/local/sbin/RPI_ShutDownReboot.hex
 #
 # https://groenholdt.net/Microcontroller/programming-(unbricking)-the-digispark.html
 #echo "Burn micronucleus to a digispark, just using a rpi"
